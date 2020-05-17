97c77a05b3f88657f32ea3103227813916cbd3e8
Bartosz Nitka <niteria@gmail.com>
2020-05-18 01:33:38 +0200

    Working version fully in Haskell, passes level 1

--- Main.hs
+++ Main.hs
@@ -2,13 +2,18 @@
 
 import Data.JSString (JSString, pack, unpack)
 import Data.Maybe (fromJust)
-import GHCJS.Foreign.Callback (Callback, asyncCallback1)
+import GHCJS.Foreign.Callback
 import GHCJS.Marshal (fromJSVal)
-import GHCJS.Types (JSVal, jsval)
+import GHCJS.Types
+import qualified JavaScript.Array as Array
+import JavaScript.Array (JSArray)
 import JavaScript.Object (Object, create, setProp)
 
-foreign import javascript unsafe "onIdle = $1" set_callback
-  :: Callback (JSVal -> IO ()) -> IO ()
+foreign import javascript unsafe "init = $1" setupInit
+  :: Callback (JSVal -> JSVal -> IO ()) -> IO ()
+
+foreign import javascript unsafe "update = $1" setupUpdate
+  :: Callback (JSVal -> JSVal -> JSVal -> IO ()) -> IO ()
 
 foreign import javascript unsafe "function() { return console; }()" getConsole
   :: IO JSVal
@@ -16,19 +21,30 @@ foreign import javascript unsafe "function() { return console; }()" getConsole
 foreign import javascript unsafe "$1.log($2)" consoleLog
   :: JSVal -> JSString -> IO ()
 
+-- XXX: don't forget to update elevator.js.externs
 foreign import javascript unsafe "$1.goToFloor($2)" goToFloor
   :: JSVal -> JSString -> IO ()
 
+foreign import javascript unsafe "$1.on($2, $3)" on
+  :: JSVal -> JSString -> Callback (IO ()) -> IO ()
+
 main :: IO ()
 main = do
-  callback <-
-    asyncCallback1 $ \elevator -> do
-      c <- getConsole
-      consoleLog c (pack "dupa")
-      goToFloor elevator (pack "0" :: JSString)
-      goToFloor elevator (pack "1" :: JSString)
-      goToFloor elevator (pack "2" :: JSString)
-      goToFloor elevator (pack "0" :: JSString)
-      consoleLog c (pack "dupa2")
-      return ()
-  set_callback callback
+  setupInit =<<
+    asyncCallback2
+      (\elevators floors -> do
+         Just (elevator:_) <- fromJSVal elevators
+         on elevator (pack "idle") =<<
+           asyncCallback
+             (do goToFloor elevator (pack "0" :: JSString)
+                 goToFloor elevator (pack "1" :: JSString)
+                 goToFloor elevator (pack "2" :: JSString)
+                 goToFloor elevator (pack "0" :: JSString)
+                 return ())
+         return ())
+  setupUpdate =<<
+    asyncCallback3
+      (\dt elevators floors -> do
+         c <- getConsole
+         -- consoleLog c (pack "update")
+         return ())
--- tpl.js
+++ tpl.js
@@ -1,20 +1,4 @@
 (function() {
-  var onIdle = function() {}
   HASKELL
-  return {
-    init: function(elevators, floors) {
-
-        let elevator = elevators[0]; // Let's use the first elevator
-
-        elevator.on("idle", function() {
-            console.log("before");
-            onIdle(elevator);
-            console.log("after");
-        });
-
-    },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
-  }
+  return { init: init, update: update }
 })()
